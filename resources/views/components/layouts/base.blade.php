9e63e155b1582993ba4a8fcf2b1652e47152afff
Anvar Nazar <anvarnasar@ymail.com>
2024-03-29 01:22:08 +0530

    icons

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -6,8 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Team Task Status' }}</title>
-    @vite(['resources/css/base.css'])
-    {{ $extra_assets }}
+    @vite(['resources/scss/style.scss', 'resources/font-awesome-pro/fontawesome-pro-6.5.1-web/css/all.css'])
 </head>
 <body>
 {{ $slot }}
--- resources/views/livewire/login-form.blade.php
+++ resources/views/livewire/login-form.blade.php
@@ -1,19 +1,20 @@
-<div class="login">
-    @slot("extra_assets")
-        @vite(['resources/css/login.css', 'resources/css/form.css', 'resources/css/button.css'])
-    @endslot
+<main class="login-page">
     <h1>Login</h1>
     <form class="form" wire:submit="save">
-        <div class="form-row">
+        <div class="form-group">
             <label for="email">Email</label>
+            <i class="fa-duotone fa-envelope"></i>
             <input type="email" id="email" name="email" placeholder="user@company.com" autofocus wire:model="email">
         </div>
-        <div class="form-row">
+        <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Sup3rSecrEtPazSW0rd$" wire:model="password">
+            <i class="fa-duotone fa-lock"></i>
+            <input type="password" id="password" name="password" placeholder="Sup3rSecrEtPazSW0rd$"
+                   wire:model="password">
         </div>
-        <div class="form-row">
-            <button type="submit" class="button-primary">Login</button>
-        </div>
+        <button type="submit" class="button-primary">
+            <i class="fa-duotone fa-right-to-bracket"></i>
+            Login
+        </button>
     </form>
-</div>
+</main>
